ea5c0d38f3e838a73dad4d2d56715605793728d8
DziejopisWawel <pawel.kurbiel@wp.pl>
2019-01-13 22:40:46 +0100

    usunałem niepotrzebny kod

--- src/sample/Point.java
+++ src/sample/Point.java
@@ -6,9 +6,8 @@ import java.awt.geom.PathIterator;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
-public class Point implements Shape {
+public class Point {
 
-    private Rectangle2D.Double area;
 
     private double x;
     private double y;
@@ -30,7 +29,6 @@ public class Point implements Shape {
         this.z = z;
         this.dimension = dimension;
 
-        area = new Rectangle2D.Double(x + dimension.width/2,y+dimension.height/2,1,1);
     }
 
     public double getZ() {
@@ -70,53 +68,4 @@ public class Point implements Shape {
     }
 
 
-    @Override
-    public boolean contains(Point2D p) {
-        return area.contains(p);
-    }
-
-    @Override
-    public boolean contains(Rectangle2D r) {
-        return area.contains(r);
-    }
-
-    @Override
-    public boolean contains(double x, double y) {
-        return area.contains(x,y);
-    }
-
-    @Override
-    public boolean contains(double x, double y, double w, double h) {
-        return area.contains(x,y,w,h);
-    }
-
-    @Override
-    public Rectangle getBounds() {
-        return area.getBounds();
-    }
-
-    @Override
-    public Rectangle2D getBounds2D() {
-        return area.getBounds2D();
-    }
-
-    @Override
-    public PathIterator getPathIterator(AffineTransform at) {
-        return area.getPathIterator(at);
-    }
-
-    @Override
-    public PathIterator getPathIterator(AffineTransform at, double flatness) {
-        return area.getPathIterator(at,flatness);
-    }
-
-    @Override
-    public boolean intersects(Rectangle2D r) {
-        return area.intersects(r);
-    }
-
-    @Override
-    public boolean intersects(double x, double y, double w, double h) {
-        return area.intersects(x, y, w, h);
-    }
 }
